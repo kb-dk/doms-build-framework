bf86526f08d4b289dc1211991bc8e8985f44c05e
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2010-02-17 14:49:08 +0000

    Rewrite and simplification of the build framework. Is now based on macrodefs, not targets. Conditional builds in almost all cases. Very much faster, as antcall are not used.

--- data/convertPath.js
+++ data/convertPath.js
@@ -27,7 +27,7 @@
  * under the License.
  */
 
-/* Part of doms build framework version BUILD.FRAMEWORK.VERSION */
+/* Part of doms build framework version 1.0.5 */
 importClass(java.io.File);
 importPackage(Packages.org.apache.tools.ant);
 importPackage(Packages.org.apache.tools.ant.types);
